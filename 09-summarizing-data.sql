1f437dd9afc47560a693def319db3f7187deb414
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-02-24 19:44:21 +0700

    finish lesson 09 - summarizing data

---
+++ 09-summarizing-data.sql
@@ -0,0 +1,53 @@
+# AVG
+
+SELECT AVG(prod_price) AS avg_price
+FROM Products;
+
+SELECT AVG(prod_price) AS avg_price
+FROM Products
+WHERE vend_id = 'DLL01';
+
+# COUNT
+
+SELECT COUNT(*) AS num_cust -- count all rows
+FROM Customers;
+
+SELECT COUNT(cust_email) AS num_cust -- count only rows where cust_email is not null
+FROM Customers;
+
+# MAX
+
+SELECT MAX(prod_price) AS max_price
+FROM Products;
+
+# MIN
+
+SELECT MIN(prod_price) AS min_price
+FROM Products;
+
+# SUM
+
+SELECT *
+FROM OrderItems;
+
+SELECT SUM(quantity) AS items_ordered
+FROM OrderItems
+WHERE order_num = 20005;
+
+SELECT SUM(item_price*quantity) AS total_price
+FROM OrderItems
+WHERE order_num = 20005;
+
+# using DISTINCT
+
+SELECT AVG(DISTINCT prod_price) AS avg_price
+FROM Products
+WHERE vend_id = 'DLL01';
+
+# combining aggregate functions
+
+SELECT COUNT(*) AS num_items,
+       MIN(prod_price) AS price_min,
+       MAX(prod_price) AS price_max,
+       AVG(prod_price) AS price_avg
+FROM Products;
