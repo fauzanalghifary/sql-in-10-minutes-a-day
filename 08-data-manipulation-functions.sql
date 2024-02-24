1d6d0efce2d9ec8f6516150319b7ccf701f68643
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-02-24 19:32:45 +0700

    finish lesson 08 - data manipulation functions

---
+++ 08-data-manipulation-functions.sql
@@ -0,0 +1,27 @@
+### Note: THIS MAY BE DIFFERENT ON EACH DBMS
+
+# Text Manipulation Functions
+
+SELECT vend_name, UPPER(vend_name) AS vend_name_upcase
+FROM Vendors
+ORDER BY vend_name;
+
+SELECT cust_name, cust_contact
+FROM Customers
+WHERE cust_contact = 'Michael Green';
+
+SELECT cust_name, cust_contact
+FROM Customers
+WHERE SOUNDEX(cust_contact) = SOUNDEX('Michael Green');
+
+# Date and Time Manipulation Functions (MySQL)
+
+SELECT order_num, order_date
+FROM Orders
+WHERE YEAR(order_date) = 2020;
+
+SELECT order_num, order_date
+FROM Orders
+WHERE MONTH(order_date) BETWEEN 2 AND 6;
+
+# Numeric Manipulation Functions
