1b0884ba1f5cb11c6de1a93ced1f403c5954d175
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-02-24 12:55:47 +0700

    init

---
+++ 02-retrieve.sql
@@ -0,0 +1,40 @@
+SELECT prod_name
+FROM Products;
+
+SELECT prod_id, prod_name, prod_price
+FROM Products;
+
+SELECT *
+FROM Products;
+
+# Retrieving Distinct Rows
+
+SELECT vend_id
+FROM Products;
+
+SELECT DISTINCT vend_id
+FROM Products;
+
+SELECT DISTINCT vend_id, prod_price FROM Products;
+
+SELECT vend_id, prod_price FROM Products;
+
+# Limiting Results
+
+SELECT prod_name
+FROM Products
+LIMIT 5;
+
+SELECT prod_name
+FROM Products
+LIMIT 5 OFFSET 5;
+
+# Comment
+
+SELECT prod_name   -- this is a comment
+FROM Products;
+
+/* SELECT prod_name, vend_id
+FROM Products; */
+SELECT prod_name
+FROM Products;
---
+++ 03-sorting.sql
@@ -0,0 +1,6 @@
+# SORTING
+
+SELECT prod_name
+FROM Products
+ORDER BY prod_name;
+
